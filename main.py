aff0333d5974ab46ea198d10f5ebfd43f44d4dda
GR7 <wintermountain2375@gmail.com>
2023-05-11 01:11:28 -0700

    update: main.py|33

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ def clean_up_sentence(sentence):
     sentence_words = [lemmatizer.lemmatize(word)  for word in sentence_words]
     return sentence_words
 
+# 
 def bag_of_words(sentence):
     sentence_words= clean_up_sentence(sentence)
     bag = [0] * len(words)
